49335a84c268d63a9f06591f395780c1cf66a5e1
cedric85 <cedricjager@hotmail.fr>
2018-05-21 10:49:34 +0200

    First commit

---
+++ Config/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+class ConfigDB {
+
+	public static function database() {
+
+	$key = [
+	'username' => 'cedri934008_3qygpy',
+	'password' => 'jfort',
+	'host' => '185.98.131.90',
+	'db_name' => 'cedri934008_3qygpy'
+	];
+
+	return $key;
+
+	}
+}
