f98d34aa9352b090985f92507996413c8090d57a
cedric85 <cedricjager@hotmail.fr>
2018-07-01 13:51:13 +0400

    refonte home.php

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -25,6 +25,7 @@ class Controller {
 
   public function home() {
     $movie = $this->Movie;
+    $films = $this->Movie->lastMovies();
     $view = require 'views/home.php';
   }
 
@@ -49,26 +50,84 @@ class Controller {
 
   public function dashboard(){
     $this->Log_in->notLogin();
-    $update = $this->Movie->updateMovie();
+    $movie = $this->Movie;
     $comment = $this->Comment->findCritics();
     $this->Comment->appCritic();
     $delete = $this->Comment->deleteCritic();
-    $req = $this->Movie->getMovie();
-    $this->Movie->deleteMovie();
     require 'views/admin/dashboard.php';
   }
 
+  // Ajout films
   public function create() {
+    $msg = array();
     $this->Log_in->notLogin();
-    $this->Movie->addMovie();
+
+    if (isset($_POST['create'])) {
+      $ids = trim($_POST['ids']);
+      $ids = explode(',', $ids);
+
+      $links = trim($_POST['links']);
+      $links = explode(',', $links);
+
+      if (sizeof($ids) > 0) {
+        foreach ($ids as $key => $id) {
+          if (!$this->Movie->isMovieExists($id)) {
+            try {
+              $movie = $this->Client->getMoviesApi()->getMovie($id);
+              $credits = $this->Client->getMoviesApi()->getCredits($id);
+              $movie['video'] = array_key_exists($key, $links) ? $links[$key] : null;
+              
+              $this->Movie->addMovie($movie, function($movie_id) use($credits) {
+                foreach ($credits['cast'] as $cast) {
+                  if (!$this->Cast->isCastExists($cast['id'])) {
+                    $this->Cast->addCast($cast, function($cast_id) use($movie_id){
+                      $this->MovieCast->addMovieCast($movie_id, $cast_id);
+                    });
+                  }
+                }
+              });
+            } catch (Exception $e) {
+              $msg[] = '<li style="color: red;">An excepetion adding movie with '.$id.' ID</li>';
+            }
+            $msg[] = '<li style="color: green;">The movie '.$id.' ID added successfully.</li>';
+          } else {
+            $msg[] = '<li style="color: red;">The movie with '.$id.' ID already exists.</li>';
+          }
+        }
+      }
+    }
     require 'views/admin/create.php';
   }
 
+  // Mise à jour films
   public function update() {
-    $this->Log_in->notLogin();
-    $this->Movie->updateMovie();
-    $post = $this->Movie->getSingle($_GET['id']);
-    require 'views/admin/update.php';
+    $msg = array();
+    if (isset($_GET['id'])) {
+      $id = $_GET['id'];
+      $movie = $this->Movie->getMovie($id);
+      if (isset($_POST['update'])) {
+        $video = $_POST['video'];
+        $this->Movie->updateMovie([$video, $id]);
+        $msg[] = '<li style="color: green;">Movie '.$movie['id'].' ID updated successfully.</li>';
+      }
+      require 'views/admin/update.php';
+    } else {
+      header('Location: index.php?p=dashboard');
+    }
+  }
+
+    // Suppression films
+
+  public function delete() {
+    if (isset($_GET['id'])) {
+      $id = $_GET['id'];
+      $movie = $this->Movie->getMovie($id);
+      $this->Movie->deleteMovie($id);
+      $this->MovieCast->deleteMovieCast($movie['movie_id']);
+      header('Location: index.php?p=dashboard');
+    } else {
+      header('Location: index.php?p=dashboard');
+    }
   }
 
   public function account() {
--- Views/home.php
+++ Views/home.php
@@ -1,163 +1,30 @@
 <div class="col-md-8 maincontent">
-
-<div class="dropdown_menu">
-  <ul id="dropdown">
-    <li><a href="#">Choisissez un genre</a>
-      <ul>
-        <li><a href="#drama">Drame</a></li>
-        <li><a href="#romantic">Comédie romantique</a></li>
-        <li><a href="#dramaticcomedy">Comédie dramatique</a></li>
-        <li><a href="#comedy">Comédie</a></li>
-        <li><a href="#thriller">Thriller</a></li>
-        <li><a href="#noir">Film noir</a></li>
-        <li><a href="#polar">Polar</a></li>
-        <li><a href="#adventure">Aventure</a></li>
-        <li><a href="#horror">Horreur</a></li>
-        <li><a href="#western">Western</a></li>
-        <li><a href="#fantastic">Fantastique</a></li>
-        <li><a href="#scifi">Science-fiction</a></li>
-        <li><a href="#historic">Historique</a></li>
-        <li><a href="#document">Documentaire</a></li>
-      </ul>
-    </li>
-  </ul>
-</div>
-
-<script>$(document).ready(function(){
-  $('.menu li').hover(function(){
-    $('ul').slideDown();
-  }, function(){
-    $('ul').slideUp();
-  });
-</script> 
-
-      <div id="drama">
-        <?php foreach ($drama as $sad) : ?>
-            <a href="index.php?p=single&id=<?= $sad->id ?>"><img src="<?= $sad->image ?>" class="thumbnailPost"><p><?= $sad->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="romantic">
-        <?php foreach ($romcom as $tears) : ?>
-            <a href="index.php?p=single&id=<?= $tears->id ?>"><img src="<?= $tears->image ?>" class="thumbnailPost"><p><?= $tears->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="dramaticcomedy">
-        <?php foreach ($dramatic as $sadlaughs) : ?>
-            <a href="index.php?p=single&id=<?= $sadlaughs->id ?>"><img src="<?= $sadlaughs->image ?>" class="thumbnailPost"><p><?= $sadlaughs->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="comedy">
-        <?php foreach ($comedies as $laughs) : ?>
-            <a href="index.php?p=single&id=<?= $laughs->id ?>"><img src="<?= $laughs->image ?>" class="thumbnailPost"><p><?= $laughs->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="thriller">
-        <?php foreach ($thrill as $excitement) : ?>
-            <a href="index.php?p=single&id=<?= $excitement->id ?>"><img src="<?= $excitement->image ?>" class="thumbnailPost"><p><?= $excitement->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="noir">
-        <?php foreach ($mystery as $suspense) : ?>
-            <a href="index.php?p=single&id=<?= $suspense->id ?>"><img src="<?= $suspense->image ?>" class="thumbnailPost"><p><?= $suspense->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="polar">
-        <?php foreach ($cop as $sherlock) : ?>
-            <a href="index.php?p=single&id=<?= $sherlock->id ?>"><img src="<?= $sherlock->image ?>" class="thumbnailPost"><p><?= $sherlock->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="adventure">
-        <?php foreach ($indiana as $jones) : ?>
-            <a href="index.php?p=single&id=<?= $jones->id ?>"><img src="<?= $jones->image ?>" class="thumbnailPost"><p><?= $jones->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="horror">
-        <?php foreach ($horror as $fear) : ?>
-            <a href="index.php?p=single&id=<?= $fear->id ?>"><img src="<?= $fear->image ?>" class="thumbnailPost"><p><?= $fear->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="western">
-        <?php foreach ($farwest as $wayne) : ?>
-            <a href="index.php?p=single&id=<?= $wayne->id ?>"><img src="<?= $wayne->image ?>" class="thumbnailPost"><p><?= $wayne->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-        <div id="fantastic">
-        <?php foreach ($strange as $wells) : ?>
-            <a href="index.php?p=single&id=<?= $wells->id ?>"><img src="<?= $wells->image ?>" class="thumbnailPost"><p><?= $wells->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-        <div id="scifi">
-        <?php foreach ($sf as $sci_fi) : ?>
-            <a href="index.php?p=single&id=<?= $sci_fi->id ?>"><img src="<?= $sci_fi->image ?>" class="thumbnailPost"><p><?= $sci_fi->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-        <div id="historic">
-        <?php foreach ($hist as $history) : ?>
-            <a href="index.php?p=single&id=<?= $history->id ?>"><img src="<?= $history->image ?>" class="thumbnailPost"><p><?= $history->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
-      <div id="document">
-        <?php foreach ($documentary as $inform) : ?>
-            <a href="index.php?p=single&id=<?= $inform->id ?>"><img src="<?= $inform->image ?>" class="thumbnailPost"><p><?= $inform->title ?></p></a>    
-        <?php endforeach; ?>
-      </div><br>
-      <hr>
-
+  <div class="row mt-5 mb-5">
+    <div class="col-md-8">
+      <div class="row">
+        <?php foreach($movie->getMovies() as $m) { ?>
+          <div class="col-lg-4 col-md-6">
+            <div class="card mb-3">
+              <div class="card-body mx-auto">
+                <a href="?p=single&id=<?php echo $m['id']; ?>" class="card-link">
+                  <img class="img-fluid" src="<?php echo $movie->getPosterPath($m['poster_path'], false, 185, 278); ?>" alt="<?php echo $m['title']; ?>">
+                  <h5 class="card-title text-center mt-3 text-dark"><?php echo $m['title']; ?></h5>
+                </a>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
 </div>
 
     <aside class="col-md-4 sidebar sidebar-right">
       <h2 id="lastmovies">Les derniers films ajoutés</h2><br>
-      <div id="slider">
-        <?php foreach ($films as $slide) : ?>
-
-        <div class="slides">
-            <a id="slide" href="index.php?p=single&id=<?= $slide->id ?>"><img src="<?= $slide->image ?>" class="thumbnailPost"><p><?= $slide->title ?></p></a>
-            <p><?= $slide->genre ?></p>
-        </div>      
-        <?php endforeach; ?>
-      </div><br>
-      <div class="row widget">
-        <div class="col-xs-12">
-          <h4>Les films les mieux notés</h4>
-          </div>
-      </div>
-      <div class="row widget">
-        <div class="col-xs-12">
-          <h4>Les films les plus populaires</h4>
+        <div id="slider">
+          <?php foreach ($films as $slide) : ?>
+            <div class="slides">
+              <a id="slide" href="index.php?p=single&id=<?= $slide->id ?>"><img src="<?= $slide->image ?>" class="thumbnailPost"><p><?= $slide->title ?></p></a>
+            </div>      
+          <?php endforeach; ?>
         </div>
-      </div>
     </aside>
-
-    <div class="container">
-      <div class="about">
-        <div class="row">
-          <div class="col-md-8">
-            <h3>À propos du Coin du cinéphile</h3>
-
-
